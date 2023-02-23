b05c7436ce4d86218d302c3966c0896ce074ee32
Anatoli Kalbasin <AKalbasin@ibagroup.eu>
2023-02-23 09:47:28 +0300

    add member name validation

--- src/main/kotlin/org/zowe/zdevops/logic/DeleteOperation.kt
+++ src/main/kotlin/org/zowe/zdevops/logic/DeleteOperation.kt
@@ -46,8 +46,9 @@ class DeleteOperation {
         }
 
         fun deleteDatasetOrMember(dsn: String, member: String, zosConnection: ZOSConnection, listener: TaskListener) {
-            if (dsn.isEmpty() && member.isEmpty()) {
-                throw AbortException(zMessages.zdevops_deleting_ds_fail_none_params())
+            isMemberNameValid(member)
+            if (dsn.isEmpty()) {
+                throw AbortException(zMessages.zdevops_deleting_ds_fail_dsn_param_empty())
             }
             val memberNotEmpty = member.isNotEmpty()
             val logMessage = if (memberNotEmpty) zMessages.zdevops_deleting_ds_member(member, dsn, zosConnection.host, zosConnection.zosmfPort)
@@ -58,6 +59,11 @@ class DeleteOperation {
                                else ZosDsn(zosConnection).deleteDsn(dsn)
             }
             listener.logger.println(successMessage)
+        }
+
+        private fun isMemberNameValid(member: String) {
+            if (member.length > 8 || member.isEmpty())
+                throw Exception(zMessages.zdevops_member_name_invalid())
         }
 
     }
